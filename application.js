f5ce5ba54910b816c8611c5f2984ab103ab853c9
Alex Rusyn <17223549+alexrusyn@users.noreply.github.com>
2021-11-07 20:48:12 +0200

    AQUA-22534 Add new API class
    * Add new API class
    * Recommendations are taken into account

--- application.js
+++ application.js
@@ -1,3 +1,5 @@
+import { API } from "./const";
+
 const func = () => {
   console.log("some cool function");
 };
@@ -9,6 +11,20 @@ class API {
 
   async getPublicData() {
     const data = await fetch(API.URL);
+    return data.json();
+  }
+}
+
+class Gitlab_API {
+  static = API.GITLAB;
+
+  async getData() {
+    const data = await fetch(Gitlab_API.URL);
+    return data.json();
+  }
+
+  async getOtherData(id) {
+    const data = await fetch(`${Gitlab_API.URL}/${id}`);
     return data.json();
   }
 }
